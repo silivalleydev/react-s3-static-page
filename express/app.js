d62e18e95cb8d89f973cf227006ffa8b0faa3414
silivalleydev <silivalleydev@gmail.com>
2021-08-07 21:03:02 +0900

    app

--- express/app.js
+++ express/app.js
@@ -6,7 +6,7 @@ const exportRouter = require('./router/export');
 const app = express()
 const port = 8080
 
-app.use(cors());
+app.use(cors({origin: true, credentials: true}));
 app.use(express.json({
     limit : "50mb"
 }));

bc77526168b2e5a0574a56de38a9888758a0b053
silivalleydev <silivalleydev@gmail.com>
2021-08-07 20:16:26 +0900

    export excel restapi

--- react/src/components/mainPage/presenter.jsx
+++ react/src/components/mainPage/presenter.jsx
@@ -12,6 +12,30 @@ const Presenter = (props) => {
           filename: "excelData_demo"
         })
       }}>Excel Export</button>
+      <button onClick={() => {
+        axios.post("http://localhost:8080/export/excel_db_data", {
+          emailto: "steve@diilabs.co.kr",
+          title: "Hello Export Excel",
+          content: "excel file",
+          columns: [
+            {header: '이름', key: 'name'},
+            {header: '전화번호', key: 'phone'},
+          ],
+          query: "SELECT * FROM member where phone=? and name=?",
+          where: ['01022222222', 'jack'],
+          filename: "excelData_demo"
+        })
+      }}>Excel SQL Export</button>
+      <button onClick={() => {
+        axios.post("http://localhost:8080/export/excel_db_data", {
+          emailto: "demo@gmail.com",
+          title: "Hello Export Excel",
+          content: "excel file",
+          query: "SELECT * FROM member where phone=? and name=?",
+          where: ['01022222222', 'jack'],
+          filename: "excelData_demo"
+        })
+      }}>Excel Only SQL Export</button>
     </div>
   );
 };
